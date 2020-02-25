adf86b48e4ee828fb8e84dc24638f6ac1efffb99
Kelvin Liang <kelvin9877@gmail.com>
2020-02-25 11:29:47 +0800

    finish setup fonts and color

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App text-center">
       <h1>Your Fitness App</h1>
-      <h3>General Text</h3>
-      <p>Lorem ipsum dolor</p>
+      <h2>General Text</h2>
+      <p>Lorem ipsum dolor. User029112</p>
       <i class="fas fa-heartbeat fa-3x"></i>
     </div>
   );
