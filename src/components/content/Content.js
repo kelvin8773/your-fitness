4add152ddb5ecda3f90bea4584782f3d41c47f9a
Kelvin Liang <kelvin9877@gmail.com>
2020-02-25 13:19:10 +0800

    finish basic UI setup

---
+++ src/components/content/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Content = () => (
+  <div className="App-content">
+
+  </div>
+)
+
+export default Content;
---
+++ src/components/top/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="App-header">
+    <button className="header-left-btn">
+      <i className="fas fa-chevron-left fa-2x"></i>
+    </button>
+    <h3 className="App-title">You.Fit</h3>
+    <button className="header-right-btn">
+      right
+    </button>
+  </div>
+)
+
+export default Header;
