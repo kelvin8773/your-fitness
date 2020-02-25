5490f60ed718402831ac8f62cf3ab583c5097432
Kelvin Liang <kelvin9877@gmail.com>
2020-02-25 14:35:03 +0800

    linter fix

--- src/components/bottom/Navbar.js
+++ src/components/bottom/Navbar.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Navbar = () => (
   <div className="App-navbar">
-    <button>
+    <button type="button">
       <i className="fas fa-plus-square fa-3x" />
       Add Data
     </button>
-    <button>
+    <button type="button">
       <i className="fas fa-chart-line fa-3x" />
       Progress
     </button>
-    <button>
+    <button type="button">
       <i className="fas fa-chart-pie fa-3x" />
       Report
     </button>
-    <button>
+    <button type="button">
       <i className="fas fa-ellipsis-h fa-3x" />
       More
     </button>
--- src/components/top/Header.js
+++ src/components/top/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = () => (
   <div className="App-header">
-    <button className="header-left-btn">
+    <button className="header-left-btn" type="button">
       <i className="fas fa-chevron-left fa-2x" />
     </button>
     <h3 className="App-title">You.Fit</h3>
-    <button className="header-right-btn">
+    <button className="header-right-btn" type="button">
       right
     </button>
   </div>
