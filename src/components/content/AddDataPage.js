d1766d5e32ffe8b04ab33995bf044c911d00e41a
Kelvin Liang <kelvin9877@gmail.com>
2020-02-27 00:44:17 +0800

    finish UI for add data page (first page) & add signout link in more page

--- src/components/content/AddDataPage.js
+++ src/components/content/AddDataPage.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { ACTIVITY_TYPE, ACTIVITY_ICONS } from '../../helpers/constant';
 
-const AddDataPage = () => (
-  <div className="addData-page">
-    Add Data.
-  </div>
-);
+const AddDataPage = () => {
+
+  return (
+    <div className="addData-page">
+      {
+        ACTIVITY_TYPE.map(activity => (
+          <div key={activity} className="addData-card">
+            <div className="icon">
+              {ACTIVITY_ICONS(activity)}
+            </div>
+            <div className="title">
+              {activity}
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
 
 export default AddDataPage;
--- src/components/content/MorePage.js
+++ src/components/content/MorePage.js
@@ -1,38 +1,69 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { sigOut } from '../../slices/statusSlice';
 
-const MorePage = () => (
-  <div className="more-page">
+const MorePage = () => {
+  const dispatch = useDispatch();
+  const { name } = useSelector(state => state.user);
+  let namePlaceHolder;
+  let sexPlaceHolder;
+  let headImagePlaceHolder;
 
-    <div className="profile-row">
-      <img src="./assets/images/kelvin_profile.jpg" alt="userHead" />
-      <div>
-        <div className="name">Kelvin Liang</div>
-        <div className="sex">Male</div>
+  switch (name) {
+    case 'Guest':
+      headImagePlaceHolder = './assets/images/gregoire_vella.jpg';
+      namePlaceHolder = 'Gregoire Vella';
+      sexPlaceHolder = 'Male';
+      break;
+    case 'Kelvin':
+      headImagePlaceHolder = './assets/images/kelvin.jpg';
+      namePlaceHolder = 'Kelvin Liang';
+      sexPlaceHolder = 'Male';
+      break;
+    default:
+      headImagePlaceHolder = './assets/images/female.jpg';
+      namePlaceHolder = 'Ella Zhang';
+      sexPlaceHolder = 'Female';
+  };
+
+  return (
+    <div className="more-page">
+      <div className="profile-row">
+        <img src={headImagePlaceHolder} alt="userHead" />
+        <div>
+          <div className="name">{namePlaceHolder}</div>
+          <div className="sex">{sexPlaceHolder}</div>
+        </div>
       </div>
-    </div>
 
-    <div className="menu-row">
-      <i className="fas fa-bullseye fa-2x" />
-      <p>Your Goal</p>
-    </div>
-    <div className="menu-row">
-      <i className="fas fa-envelope fa-2x" />
-      <p>Mailing list</p>
-    </div>
-    <div className="menu-row">
-      <i className="fas fa-user fa-2x" />
-      <p>Your profile</p>
-    </div>
-    <div className="menu-row">
-      <i className="fas fa-cogs fa-2x" />
-      <p>Settings</p>
-    </div>
-    <div className="menu-row">
-      <i className="fas fa-info-circle fa-2x" />
-      <p>Help</p>
-    </div>
+      <div className="menu-row">
+        <i className="fas fa-bullseye fa-2x" />
+        <p>Your Goal</p>
+      </div>
+      <div className="menu-row">
+        <i className="fas fa-envelope fa-2x" />
+        <p>Mailing list</p>
+      </div>
+      <div className="menu-row">
+        <i className="fas fa-user fa-2x" />
+        <p>Your profile</p>
+      </div>
+      <div className="menu-row">
+        <i className="fas fa-cogs fa-2x" />
+        <p>Settings</p>
+      </div>
+      <div className="menu-row">
+        <i className="fas fa-info-circle fa-2x" />
+        <p>Help</p>
+      </div>
+      <div className="menu-row" onClick={() => dispatch(sigOut())}>
+        <i className="fas fa-sign-out-alt fa-2x" />
+        <p>Sign Out</p>
+      </div>
 
-  </div>
-);
+    </div>
+  );
+}
+
 
 export default MorePage;
