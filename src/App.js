d6e857203024b98a0ea4222ac0b5fce834b14752
Kelvin Liang <kelvin9877@gmail.com>
2020-02-26 19:05:54 +0800

    fix deployment issue again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import LoginPage from './components/LoginPage';
+import LoginPage from './components/Login';
 import AppPage from './components/AppPage';
 
 function App() {
