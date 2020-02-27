d709e4b676607f504745f95f41d982cbe16df30a
Kelvin Liang <kelvin9877@gmail.com>
2020-02-27 21:51:44 +0800

    add header back link and update home welcome page

--- src/components/content/HomePage.js
+++ src/components/content/HomePage.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { setCurrentPage } from '../../slices/statusSlice';
 
 const HomePage = () => {
   const userName = useSelector(state => state.user.name);
+  const dispatch = useDispatch();
 
   return (
     <div className="home-page">
+      <h2>
+        Hi, {userName}, Welcome!
+      </h2>
+      <i className="fas fa-running fa-2x" />
+      <h3>What is your Goal today? </h3>
 
-      <h3>
-Welcome,
-        {userName}
-!
-      </h3>
-      <p>What is your Goal today?</p>
+      <Link
+        to="/addData"
+        onClick={() => dispatch(setCurrentPage('Add Activities'))}
+        className="btn"
+      >
+        Let's Start Now!
+      </Link>
+
     </div>
   );
 };
--- src/components/top/Header.js
+++ src/components/top/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+
 import { setCurrentPage } from '../../slices/statusSlice';
 
 const Header = () => {
@@ -9,17 +10,24 @@ const Header = () => {
 
   return (
     <div className="App-header">
-      <button className="header-left-btn" type="button">
+      <Link
+        to="/"
+        className={currentPage === 'You.Fit' ? "left-btn-off" : "left-btn-on"}
+        onClick={() => dispatch(setCurrentPage('You.Fit'))}
+      >
         <i className="fas fa-chevron-left fa-2x" />
-      </button>
+      </Link>
       <Link to="/" onClick={() => dispatch(setCurrentPage('You.Fit'))}>
         <h3 className="App-title">
           {currentPage || 'You.Fit'}
         </h3>
       </Link>
-      <button className="header-right-btn" type="button">
+      <Link
+        to="/"
+        className="right-btn-off"
+      >
         right
-      </button>
+      </Link>
 
     </div>
   );
