d583340a56ebae03d694b226d80c174b173c20a4
Kelvin Liang <kelvin9877@gmail.com>
2020-03-04 18:40:07 +0800

    improve ui of progress page

---
+++ src/components/content/progress/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProgressPage = () => {
+
+  return (
+    <div className="progress-page">
+      <h3>This page is planning ...</h3>
+      <i className="fas fa-pencil-ruler"></i>
+    </div>
+  );
+}
+
+
+export default ProgressPage;
